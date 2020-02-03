2dccc73f15f4a56818d942c60791146e89dbe09c
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-02-03 19:17:12 +0900

    Add beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import TodoList from 'components/TodoList';
 import { TodoState } from 'reducers/todoReducer';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const App: React.FC<TodoState> = ({ todo }) => {
+  const onDragEnd = () => {
+
+  }
   return (
-    <div className="App">
-      <div style={styles.listsContainer}>
-        {todo.map(ele => <TodoList key={ele.id} {...ele}/>)}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <div style={styles.listsContainer}>
+          {todo.map(ele => <TodoList key={ele.id} {...ele}/>)}
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import TodoCard from './TodoCard';
 import { TodoElement } from 'reducers/todoReducer';
 import TodoButton from 'containers/TodoButton';
+import { Droppable } from "react-beautiful-dnd";
 
 const TodoList: React.FC<TodoElement> = ({ title, cards, id }) => {
   return (
-  <div style={styles.container}>
-    <h4>{title}</h4>
-    {cards.map(card => (<TodoCard key={card.id} {...card}/>))}
-    <TodoButton id={id} />
-  </div>);
+    <Droppable droppableId={String(id)}>
+      {provided => (
+        <div {...provided.droppableProps} ref={provided.innerRef} style={styles.container}>
+          <h4>{title}</h4>
+          {cards.map((card, index) => (
+            <TodoCard
+              key={card.id}
+              id={card.id}
+              index={index}
+              text={card.text}
+            />
+          ))}
+          <TodoButton id={id} />
+        </div>
+      )}
+    </Droppable>
+  );
 }
 
 const styles = {
